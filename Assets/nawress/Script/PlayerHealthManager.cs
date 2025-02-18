80754bb2b38f462d4ed8394172a54ba5c96dd48b
nawres <nawres@gmail.com>
2025-02-18 12:04:17 +0100

    zefet independent Woman

--- Assets/nawress/Script/PlayerHealthManager.cs
+++ Assets/nawress/Script/PlayerHealthManager.cs
@@ -5,24 +5,16 @@ using UnityEngine.UI;
 
 public class PlayerHealthManager : MonoBehaviour
 {
-
-
-   
-    
     #region Private Variables
     [SerializeField] private float currentHealth;
     [SerializeField] private float maxHealth = 100f;
     [SerializeField] private float damageAmount = 3F;
-
     [SerializeField] private GameOverScreen gameOverScreen;
-
-
     [SerializeField] private SurvivalTimer survivalTimer;
     [SerializeField] private SpriteRenderer spriteRenderer;
     [SerializeField] private Sprite normalSprite;
     [SerializeField] private Sprite hitSprite;
     [SerializeField] private float hitEffectDuration = 0.2f;
-
     #endregion
 
     public Slider Healthslider; // L'image remplissable repr�sentant la barre de sant�
@@ -57,73 +49,38 @@ public class PlayerHealthManager : MonoBehaviour
 
     void Update()
     {
-
-        // Exemple pour tester la prise de d�g�ts
-
         Debug.Log(currentHealth);
         if (currentHealth <= 0f)
         {
             Debug.Log("IIIIIIWAH");
-           PlayerDeath();
+            PlayerDeath();
+        }
 
         if (Input.GetKeyDown(KeyCode.T))
         {
             TakeDamage(damageAmount);
-
         }
     }
 
-    public void TakeDamage(float damageToTake)
+    void TakeDamage(float damageToTake)
     {
         currentHealth -= damageToTake;
-
-        currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth); // Emp�che les valeurs n�gatives
-       /* if (currentHealth <= 0)
-
-        currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth); // Emp�che les valeurs n�gatives
-        
-        
-        
-        if (currentHealth <= 0)
-
-        {
-            survivalTimer.PlayerDied();
-            gameObject.SetActive(false);
-        }
-
-        UpdateHealthBar();*/
-
+        currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
         Healthslider.value = currentHealth;
-        
-
     }
 
-   
-    private void OnTriggerEnter2D(Collider2D collision)
+    void OnTriggerEnter2D(Collider2D collision)
     {
-
-        if (healthBar != null)
-
-        if (collision.tag == "Enemy")
-
+        if (collision.CompareTag("Enemy"))
         {
             TakeDamage(damageAmount);
         }
     }
 
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        TakeDamage(damageAmount);
-    }
-    public void PlayerDeath()
+    void PlayerDeath()
     {
         // Quand le joueur meurt
         gameOverScreen.ShowGameOver();
     }
-
-
-}
-
 }
 
