3013b088909febe65aa7e074812be9036da819cd
nawres <nawres@gmail.com>
2025-03-01 11:52:25 +0100

    2type enemy spawn just need bch te5ou level(+Bachar)

--- Assets/nawress/Script/EnemyTypes.cs
+++ Assets/nawress/Script/EnemyTypes.cs
@@ -1,14 +1,1 @@
-using UnityEngine;
-
-public class EnemyTypes : MonoBehaviour
-{
-    [System.Serializable]
-    public class EnemyType
-    {
-        public GameObject enemyPrefab;
-        public float spawnWeight = 1f; // Higher weight = more common
-        public float minSpawnTime = 0f; // When this enemy starts appearing (in seconds)
-    }
-
-    public EnemyType[] enemyTypes;
-} 
+ 
